c36fdebecd9e81202520fbe7de74b1eed9b4f268
Markus Ullmann <mail@markus-ullmann.de>
2023-01-15 15:05:32 +0100

    Add in lxml dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 import sys
 
 build_options = {
-    'packages': ['multiprocessing'],
+    'packages': ['multiprocessing', 'lxml'],
     'excludes': ['tkinter'],
     'include_files':['lib/']
 }
