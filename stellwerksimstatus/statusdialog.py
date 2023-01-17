75fdfeb7ed40f534b0756b8c2042be32b002ec46
Markus Ullmann <mail@markus-ullmann.de>
2023-01-17 21:00:04 +0100

    Introduce sentry for runtime stability check

--- stellwerksimstatus/statusdialog.py
+++ stellwerksimstatus/statusdialog.py
@@ -1,3 +1,13 @@
+import sentry_sdk
+sentry_sdk.init(
+    dsn="https://537836497cdd4f9c8929b23b9fc07b25@sentry.hbch.de/3",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
+
 import sys
 import time
 
